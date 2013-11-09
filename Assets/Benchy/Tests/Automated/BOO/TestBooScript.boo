f57eefc705b2d8cfa1ef20fab2788326abd19e67
Tony Puccinelli <tony.puccinelli@gmail.com>
2013-11-10 00:30:26 +0100

    imported Benchy to the project

---
+++ Assets/Benchy/Tests/Automated/BOO/TestBooScript.boo
@@ -0,0 +1,12 @@
+import UnityEngine
+
+class TestBooScript (MonoBehaviour): 
+
+	def Start ():
+		pass
+	
+	def Update():
+		BooAutoTest()
+		
+	def BooAutoTest():
+		transform.Rotate(0, (5 * Time.deltaTime), 0)
---
+++ Assets/Benchy/Tests/Automated/CS/TestMethod.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class TestMethod : MonoBehaviour {
+	
+	public bool callOtherMethod;
+	 
+	void Awake(){
+		
+	}
+	
+	void Start () {
+		PublicMethod();
+		if (callOtherMethod) OtherMethod();
+	}
+	
+	void Update() {
+		AnotherMethod(2);
+	}
+	
+	public void PublicMethod(){
+		GetInstanceID();
+		AnotherMethod(2);
+	}
+		
+	public void AnotherMethod(int x){
+	}
+	
+	public void OtherMethod(){
+		
+	}
+	
+	private const KeyCode rightKeyCode        = KeyCode.D;
+	
+	void FixedUpdate(){
+		Ray ray = Camera.mainCamera.ScreenPointToRay(Vector3.zero);
+		Physics.Raycast(ray);
+	}
+}
